8b6f10654df3f67d341d44a41b3181c49bcceb3a
Serious-Sam-Frg <abdullaevbatyr416@gmail.com>
2022-09-09 09:13:06 +0500

    Подключение с рабочего

--- images/js/main.js
+++ images/js/main.js
@@ -205,11 +205,11 @@ $(function() {
     let defaultVoice;
 
     speechSynthesis.onvoiceschanged = () => {
-    voices = speechSynthesis.getVoices();
-    defaultVoice = voices.find((voice) => voice.name === "Google русский");
+        voices = speechSynthesis.getVoices();
+        defaultVoice = voices.find((voice) => voice.name === "Google русский");
 
-    wrapper.addEventListener("click", handleClick);
-    window.addEventListener("keydown", handleKeydown);
+        wrapper.addEventListener("click", handleClick);
+        window.addEventListener("keydown", handleKeydown);
     };
 
     const PLAY = "play";
@@ -217,60 +217,60 @@ $(function() {
     const RESUME = "resume";
 
     function handleClick({ target }) {
-    switch (target.className) {
-        case PLAY:
-        speechSynthesis.cancel();
+        switch (target.className) {
+            case PLAY:
+            speechSynthesis.cancel();
 
-        const { textContent } = target.nextElementSibling;
+            const { textContent } = target.nextElementSibling;
 
-        textContent.split(".").forEach((text) => {
-            const trimmed = text.trim();
-            if (trimmed) {
-            const U = getUtterance(target, text);
-            speechSynthesis.speak(U);
-            }
-        });
-        break;
-        case PAUSE:
-        target.className = RESUME;
-        speechSynthesis.pause();
-        break;
-        case RESUME:
-        target.className = PAUSE;
-        speechSynthesis.resume();
-        break;
-        default:
-        break;
-    }
+            textContent.split(".").forEach((text) => {
+                const trimmed = text.trim();
+                if (trimmed) {
+                    const U = getUtterance(target, text);
+                    speechSynthesis.speak(U);
+                }
+            });
+            break;
+            case PAUSE:
+            target.className = RESUME;
+            speechSynthesis.pause();
+            break;
+            case RESUME:
+            target.className = PAUSE;
+            speechSynthesis.resume();
+            break;
+            default:
+            break;
+        }
     }
 
     function handleKeydown({ code }) {
-    switch (code) {
-        case "Escape":
-        return speechSynthesis.cancel();
-        default:
-        break;
-    }
+        switch (code) {
+            case "Escape":
+            return speechSynthesis.cancel();
+            default:
+            break;
+        }
     }
 
     function getUtterance(target, text) {
-    const U = new SpeechSynthesisUtterance(text);
-    U.voice = defaultVoice;
-    U.lang = defaultVoice.lang;
-    U.volume = 1;
-    U.rate = 1;
-    U.pitch = 1;
+        const U = new SpeechSynthesisUtterance(text);
+        U.voice = defaultVoice;
+        U.lang = defaultVoice.lang;
+        U.volume = 1;
+        U.rate = 1;
+        U.pitch = 1;
 
-    U.onstart = () => {
-        console.log("Started");
-        target.className = PAUSE;
-    };
-    U.onend = () => {
-        console.log("Finished");
-        target.className = PLAY;
-    };
-    U.onerror = (err) => console.error(err);
+        U.onstart = () => {
+            console.log("Started");
+            target.className = PAUSE;
+        };
+        U.onend = () => {
+            console.log("Finished");
+            target.className = PLAY;
+        };
+        U.onerror = (err) => console.error(err);
 
-    return U;
+        return U;
     }
 });
